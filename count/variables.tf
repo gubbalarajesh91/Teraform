25a0300045277b1ce22f9c94adb572fc95c8a2d3
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-13 03:41:10 +0530

    Terraform

--- count/variables.tf
+++ count/variables.tf
@@ -1,15 +1,4 @@
-variable "image_id" {
-  type = string
-  default = "ami-090252cbe067a9e58"
-  description = "RHEL-9-Practice AMI ID"
-}
-
-variable "instance_type" {
-  type = string
-  default = "t3.micro"
-  description = "AWS Instance"
-}
-
-variable "instance_name" {
-    default = "backend"
+variable "instance_names" {
+  type = list
+  default = ["db", "backend", "frontend"]
 }
