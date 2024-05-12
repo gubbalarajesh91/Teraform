300f6218e168372cf5f6ec1c6d8adc32256984c5
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-13 02:58:05 +0530

    Terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,10 +1,30 @@
+#resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
-  ami = var.image_id
+  ami           = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type = "t3.micro"
 
-  #instance_type = "t3.micro"
-  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
-  #leftsdie keys are arguments and right side are values
+  tags = {
+    Name = "db"
+  }
+}
 
-  
-  
+resource "aws_instance" "backend" {
+  ami           = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
 }
