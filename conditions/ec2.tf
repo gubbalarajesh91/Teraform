2b03000a6c96ba77ca00c4c55909eea142c588d5
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-13 02:47:07 +0530

    Terraform

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "db" {
   ami = var.image_id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
   #instance_type = "t3.micro"
-  instance_type = var.instance_type == "db" ? "t3.small" : "t3.micro"
+  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
   #leftsdie keys are arguments and right side are values
 
   
