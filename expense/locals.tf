8bc9a29890e79467d6e6765cccaaa891d1f3be1a
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-14 01:06:05 +0530

    Terraform

--- expense/locals.tf
+++ expense/locals.tf
@@ -1,9 +1,10 @@
-locals {
-    record_name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
-    record_value = var.instance_names[count.index] == "frontend" ? [aws_instance.Expense[count.index].public_ip] : [aws_instance.Expense[count.index].private_ip]
-}
+# locals {
+#     record_name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
+#     record_value = var.instance_names[count.index] == "frontend" ? [aws_instance.Expense[count.index].public_ip] : [aws_instance.Expense[count.index].private_ip]
+# }
 
-
+#local count.index not work
+#count.index will work if ther is count = "number" available in same .tf file
 #record_value = var.instance_names[count.index] == "frontend" ? [aws_instance.Expense[count.index].public_ip] : [aws_instance.Expense[count.index].private_ip]
 
 #[aws_instance.Expense[count.index].public_ip]--- Resource count
--- expense/r53.tf
+++ expense/r53.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
-  #name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
+  name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
   #Interpolation "Hello, ${var.name}!"
-  name = local.record_name
+  
   type    = "A"
   ttl     = 1
-  records = local.record_value
+  records = var.instance_names[count.index] == "frontend" ? [aws_instance.Expense[count.index].public_ip] : [aws_instance.Expense[count.index].private_ip]
   allow_overwrite = true #if records already exist this will overwite
 }
