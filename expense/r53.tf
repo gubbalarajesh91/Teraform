2164a0e0a6ac1a282adb07e0f7569242c04f7a2c
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-13 17:24:12 +0530

    Terraform

--- expense/r53.tf
+++ expense/r53.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
   name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
+  #Interpolation "Hello, ${var.name}!"
   type    = "A"
   ttl     = 1
   records = [aws_eip.lb.public_ip]
