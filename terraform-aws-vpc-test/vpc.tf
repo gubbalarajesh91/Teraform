4ace2dc380c93163b06755c385596677e0372918
Rajesh Gubbala <gubbala.rajesh91@gmail.com>
2024-05-26 20:48:28 +0530

    VPC

--- terraform-aws-vpc-test/vpc.tf
+++ terraform-aws-vpc-test/vpc.tf
@@ -1,6 +1,7 @@
 module "vpc" {
 
-    source = "../terraform-aws-vpc"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/gubbalarajesh91/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
